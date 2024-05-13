2f3eabcf7b45b586c7562bb84e4e22bdfe9f6fe1
Inti <intimartinezfloresicube@gmail.com>
2024-05-13 13:11:38 -0600

    first commit

---
+++ V1--Proyecto-que-combine-IoT-e-IA-master/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { MatButtonModule } from '@angular/material/button'
+
+@NgModule({
+  imports: [
+    AppComponent,
+    MatButtonModule
+  ],
+  declarations: [
+
+  ]
+})
+export class AppModule { }
